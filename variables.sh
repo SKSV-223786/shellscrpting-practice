70a3c710a04965ac4a2bedeb6b338c1dabfb44fd
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-20 08:29:35 +0530

    updated

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#variables
+friend1=sekhar
+friend2=saidavali
+echo "$friend1 : hi, $friend2 How are you?"
+echo "$friend2 : hi friend1, Fine what about you?"
+echo "$friend1: i am good, which language you are learning?"
+echo "$friend2:: i am learning shell scripting"
+echo "$friend1:: oh great! all the best"
+echo "$friend2 :: Thank you!"
+echo "$friend1:: you are welcome!"
