1f15d017667391cdca9771309611704cc05738fb
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-20 10:21:15 +0530

    Arguments

--- readparameters.sh
+++ readparameters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "please enter your PIN Number"
-read PIN_NUMBER # here pin number is a variable
+read -s PIN_NUMBER # here pin number is a variable
 
 echo "PIN entered is: $PIN_NUMBER"
