769af31a6d016ae135c4d130bc4abe93c8f934cc
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-20 09:39:22 +0530

    Arguments

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1::welcome to Alaska, $person2"
+echo "$person2:: thank you $person1, Finally not on a zoom webinar"
+echo "$person1:: so, Do we have a Deal?"
+echo "$person2:: yes, if you make ukraine joins Russia"
