86a00a9fca4dd640893d66063c29e3a0ce47c9c1
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-20 11:26:37 +0530

    env-variable

---
+++ 06.special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
