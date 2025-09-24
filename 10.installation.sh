7fb9872eaecefcc50ea2bebf81aa2cf1b9913af1
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-24 11:09:36 +0530

    condtions

---
+++ 10.installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(ID -u)
+
+if [ $USERID -ne 0]; then
+    echo "Error: Please run this script with root privilege"
+fi
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Error: Installing My sql is Failure"
+
+else 
+    echo "installing mysql is success"
+fi
