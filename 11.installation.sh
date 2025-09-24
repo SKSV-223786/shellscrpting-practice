a51fbd3898049f8dced59d8de844105ca706bca9
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-24 11:46:04 +0530

    condtions

--- 11.installation.sh
+++ 11.installation.sh
@@ -6,34 +6,24 @@ if [ $USERID -ne 0 ]; then
     echo "Error: Please run this script with root privilege"
     exit 1 # failure is other than 0
 fi
-dnf install mysql -y
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Error: Installing $1 is Failure"
+        exit 2 
 
-if [ $? -ne 0 ]; then
-    echo "Error: Installing My sql is Failure"
-    exit 2 
-
-else 
-    echo "installing mysql is success"
-fi
+    else 
+        echo "installing $1 is success"
+    fi
+}
+#installation of MYSQL
+dnf remove mysql -y
+VALIDATE $? "mysql"
 
 #installation of Nginx
 
-dnf install nginx -y
-
-if [ $? -ne 0 ]; then
-    echo "installation of nginx is failure"
-    exit 1
-else
-    echo "installation of nginx is success"
-fi
-
+dnf remove nginx -y
+VALIDATE $? "nginx"
 #installation of mongodb-mogosh
 
-dnf install python3 -y
- 
-if [ $? -ne 0 ]; then
-    echo "ERROR: Installing python3 is failure"
-    exit 1
-else    
-    echo "installation of python3 is success"
-fi
+dnf remove python3 -y
+VALIDATE $? "python3"
