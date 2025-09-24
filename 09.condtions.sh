0fa59b7eeb642ba1f3610d24eb1975aea64e4dfc
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-24 06:53:42 +0530

    condtions

---
+++ 09.condtions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter the number:"
+read NUMBER
+
+if [ $(($NUMBER % 2))]; then
+    echo "given number $NUMBER is Even"
+
+else
+    echo "given number $NUMBER is Odd"
+fi
