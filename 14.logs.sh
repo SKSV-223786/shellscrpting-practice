db393bb7735b22d235bbc2bbb4efa56fcc062574
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-24 13:19:35 +0530

    condtions

---
+++ 14.logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"
+if [ $USERID -ne 0 ]; then
+    echo " $R Error: Please run this script with root privilege"
+    exit 1 # failure is other than 0
+fi
+
+#installation of MYSQL
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "mysql"
+else
+    echo -e "mysql is already exist.....$Y SKIPPING $N"
+fi
+
+#installation of Nginx
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx is already exist....$Y Skipping $N"
+fi
+#installation of mongodb-mogosh
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e  "nginx is already exist....$Y Skipping $N"
+fi
+
+
+
